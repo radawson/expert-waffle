d0d81b905fba8a28501a7145d6af7a52fe451b23
Marco <114459798+marcodillanes@users.noreply.github.com>
2023-01-14 17:21:49 -0800

    resubmission

--- interactive-map/app.js
+++ interactive-map/app.js
@@ -106,7 +106,3 @@ const options = {
 	}
   };
   
-  fetch('https://api.foursquare.com/v3/places/search', options)
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
